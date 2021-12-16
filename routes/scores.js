106741ecff2a6b1dbcd5c4e3ae02038e957445b8
dekimpethomas1 <thomas.dekimpe@student.vinci.be>
2021-12-16 12:41:15 +0100

    add - ajout des scores (sans points) / récup les scores d'un joueur ou associés à un texte

--- routes/scores.js
+++ routes/scores.js
@@ -9,5 +9,33 @@ router.get("/", function(req, res){
     return res.json(scoresModel.getAll());
 })
 
+// GET /texts : read all the scores of player
+router.get("/player/:playerId", function(req, res){
+    const scores = scoresModel.getScoresByPlayer(req.params.playerId);
+    if (!scores) return res.status(404).end();
+
+    return res.json(scores);
+})
+
+// GET /texts : read all the scores of text
+router.get("/text/:textId", function(req, res){
+    const scores = scoresModel.getScoresByText(req.params.textId);
+    if (!scores) return res.status(404).end();
+
+    return res.json(scores);
+})
+
+router.post("/add", function(req, res){
+    if(
+        !req.body ||
+        !req.body.hasOwnProperty("player") ||
+        !req.body.hasOwnProperty("text")
+    )
+        return res.status(400).end();
+
+    const score = scoresModel.addOne(req.body);
+    
+    return res.json(score);
+})
 
 module.exports = router;
